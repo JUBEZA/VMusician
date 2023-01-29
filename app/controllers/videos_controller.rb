f72da66b44cbe995c4c077acc1e77861d6414e17
Juan Bezanilla <juanbezanillaz@gmail.com>
2023-01-29 20:26:02 -0300

    Crud fixed

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,7 @@
 class VideosController < ApplicationController
 
   def home
-    
+
   end
 
   def index
@@ -19,6 +19,16 @@ class VideosController < ApplicationController
   def create
     @video = Video.new(video_params)
     @video.save
+    redirect_to video_path(@video)
+  end
+
+  def edit
+    @video = Video.find(params[:id])
+  end
+
+  def update
+    @video = Video.find(params[:id])
+    @video.update(video_params)
     redirect_to video_path(@video)
   end
 
